cb8a102e839c7f3dff1792471d9eb5c0d02cc802
Prashanna Paudel <prashannapaudel2@gmail.com>
2020-02-06 11:02:44 +0545

    pmp

--- BST.h
+++ BST.h
@@ -3,8 +3,8 @@ class BST
 public:
     virtual void add(int data)=0;
     virtual void preOrderTraversal()=0;
-    virtual void search(int data)=0;
+    //virtual void search(int data)=0;
 
-    virtual int max_e() = 0;
-    virtual int min()=0;
+    //virtual int max_e() = 0;
+    //virtual int min()=0;
 };
